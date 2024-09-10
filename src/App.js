83770f597b745e47694207cbc874dedcab5dba74
Shraddha Shrivastav <shrivastavshraddha75@gmail.com>
2024-09-10 23:55:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Contact from './pages/contact';
+import About from './pages/about';
+import Services from './pages/services';
+import Clients from './pages/clients';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={ <Home /> }></Route>
+        <Route path='/contact' element={ <Contact  /> }></Route>
+        <Route path='/about' element={ <About />}></Route>
+        <Route path='/services' element={ <Services /> }></Route>
+        <Route path='/clients' element={ <Clients /> } ></Route>
+        
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
